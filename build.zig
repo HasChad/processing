5719651aa79d9b5aa0e37d30637638d57de29841
HasChad <troll.yahya@gmail.com>
2026-08-15 01:16:11 +0300

    - first commit

---
+++ build.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+
+const sketches = [_][]const u8{"rain"};
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
+    for (sketches) |name| {
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(b.fmt("src/{s}/main.zig", .{name})),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+
+        exe.root_module.linkLibrary(raylib_artifact);
+        exe.root_module.addImport("raylib", raylib);
+        exe.root_module.addImport("raygui", raygui);
+        b.installArtifact(exe);
+
+        exe.use_lld = true;
+        exe.use_llvm = true;
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        const run_step = b.step(name, b.fmt("Run {s}", .{name}));
+        run_step.dependOn(&run_cmd.step);
+
+        const check = b.step("check", "Check if it compiles");
+        check.dependOn(&exe.step);
+    }
+}
---
+++ src/rain/main.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+const screenWidth = 800;
+const screenHeight = 600;
+const bg_color = rl.Color{ .a = 255, .r = 25, .g = 23, .b = 36 };
+const rain_color = rl.Color{ .a = 155, .r = 144, .g = 140, .b = 170 };
+
+const Drop = struct {
+    x: f32,
+    y: f32,
+    vy: f32,
+
+    pub fn init(rand: std.Random) Drop {
+        return Drop{
+            .x = rand.float(f32) * screenWidth,
+            .y = rand.float(f32) * 500 - 700,
+            .vy = rand.float(f32) * 1000 + 500,
+        };
+    }
+
+    pub fn draw(self: Drop) void {
+        var color = rain_color;
+
+        const alpha_f = (self.vy / 1500.0) * 255;
+        color.a = @intFromFloat(alpha_f);
+
+        rl.drawRectangle(
+            @intFromFloat(self.x),
+            @intFromFloat(self.y),
+            @intFromFloat(self.vy / 300),
+            @intFromFloat(self.vy / 10),
+            color,
+        );
+    }
+
+    pub fn move(self: *Drop, rand: std.Random) void {
+        self.y += self.vy * rl.getFrameTime();
+
+        if (self.y > screenHeight) {
+            self.* = Drop.init(rand);
+        }
+    }
+};
+
+pub fn main(init: std.process.Init) anyerror!void {
+    const io = init.io;
+    const rng_impl: std.Random.IoSource = .{ .io = io };
+    const rand = rng_impl.interface();
+
+    rl.initWindow(screenWidth, screenHeight, "rain");
+    defer rl.closeWindow();
+    rl.setTargetFPS(60);
+
+    var drop: [200]Drop = undefined;
+
+    for (&drop) |*single| {
+        single.* = Drop.init(rand);
+    }
+
+    while (!rl.windowShouldClose()) {
+        for (&drop) |*single| {
+            single.*.move(rand);
+        }
+
+        // Draw
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(bg_color);
+
+        for (&drop) |*single| {
+            single.*.draw();
+        }
+    }
+}
